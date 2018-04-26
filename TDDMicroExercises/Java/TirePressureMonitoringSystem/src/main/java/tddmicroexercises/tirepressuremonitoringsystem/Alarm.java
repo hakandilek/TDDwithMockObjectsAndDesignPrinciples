109d67a8f435e2b40629767122d602368259790d
Hakan Dilek <hakandilek@gmail.com>
2018-04-26 10:16:11 +0200

    reformat changed classes

--- TDDMicroExercises/Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ TDDMicroExercises/Java/TirePressureMonitoringSystem/src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -1,38 +1,34 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-public class Alarm
-{
-    public static final double LOW_PRESSURE_TRESHOLD = 17;
-    public static final double HIGH_PRESSURE_TRESHOLD = 21;
+public class Alarm {
+  public static final double LOW_PRESSURE_TRESHOLD = 17;
+  public static final double HIGH_PRESSURE_TRESHOLD = 21;
 
-    private final Sensor sensor;
+  private final Sensor sensor;
 
-    private boolean alarmOn = false;
+  private boolean alarmOn = false;
 
-    public Alarm(Sensor sensor) {
-	this.sensor = sensor;
+  public Alarm(Sensor sensor) {
+    this.sensor = sensor;
+  }
+
+  public Alarm() {
+    this(new Sensor());
+  }
+
+  public Sensor getSensor() {
+    return sensor;
+  }
+
+  public void check() {
+    double psiPressureValue = sensor.popNextPressurePsiValue();
+
+    if (psiPressureValue < LOW_PRESSURE_TRESHOLD || HIGH_PRESSURE_TRESHOLD < psiPressureValue) {
+      alarmOn = true;
     }
+  }
 
-    public Alarm() {
-	this(new Sensor());
-    }
-
-    public Sensor getSensor() {
-        return sensor;
-    }
-
-    public void check()
-    {
-        double psiPressureValue = sensor.popNextPressurePsiValue();
-
-        if (psiPressureValue < LOW_PRESSURE_TRESHOLD || HIGH_PRESSURE_TRESHOLD < psiPressureValue)
-        {
-            alarmOn = true;
-        }
-    }
-
-    public boolean isAlarmOn()
-    {
-        return alarmOn;
-    }
+  public boolean isAlarmOn() {
+    return alarmOn;
+  }
 }
--- TDDMicroExercises/Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ TDDMicroExercises/Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -10,59 +10,59 @@ import org.junit.Test;
 
 public class AlarmTest {
 
-    Sensor sensor;
-    Alarm alarm;
+  Sensor sensor;
+  Alarm alarm;
 
-    @Before
-    public void setup() {
-	sensor = mock(Sensor.class);
-	alarm = new Alarm(sensor);
+  @Before
+  public void setup() {
+    sensor = mock(Sensor.class);
+    alarm = new Alarm(sensor);
+  }
+
+  @Test
+  public void checkNominalPressure() throws Exception {
+    for (double pressure = Alarm.LOW_PRESSURE_TRESHOLD; pressure <= Alarm.HIGH_PRESSURE_TRESHOLD; pressure++) {
+      when(sensor.popNextPressurePsiValue()).thenReturn(pressure);
+      alarm.check();
+      assertFalse(alarm.isAlarmOn());
     }
+  }
 
-    @Test
-    public void checkNominalPressure() throws Exception {
-	for (double pressure = Alarm.LOW_PRESSURE_TRESHOLD; pressure <= Alarm.HIGH_PRESSURE_TRESHOLD; pressure++) {
-	    when(sensor.popNextPressurePsiValue()).thenReturn(pressure);
-	    alarm.check();
-	    assertFalse(alarm.isAlarmOn());
-	}
-    }
+  @Test
+  public void checkPressureTooLow() throws Exception {
+    when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.LOW_PRESSURE_TRESHOLD - 1);
+    alarm.check();
+    assertTrue(alarm.isAlarmOn());
+  }
 
-    @Test
-    public void checkPressureTooLow() throws Exception {
-	when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.LOW_PRESSURE_TRESHOLD - 1);
-	alarm.check();
-	assertTrue(alarm.isAlarmOn());
-    }
+  @Test
+  public void checkPressureTooHigh() throws Exception {
+    when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.HIGH_PRESSURE_TRESHOLD + 1);
+    alarm.check();
+    assertTrue(alarm.isAlarmOn());
+  }
 
-    @Test
-    public void checkPressureTooHigh() throws Exception {
-	when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.HIGH_PRESSURE_TRESHOLD + 1);
-	alarm.check();
-	assertTrue(alarm.isAlarmOn());
-    }
+  @Test
+  public void alarmShouldBeOnOnceTriggeredHigh() throws Exception {
+    when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.HIGH_PRESSURE_TRESHOLD - 1);
+    alarm.check();
+    when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.HIGH_PRESSURE_TRESHOLD + 1);
+    alarm.check();
+    when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.HIGH_PRESSURE_TRESHOLD);
+    alarm.check();
 
-    @Test
-    public void alarmShouldBeOnOnceTriggeredHigh() throws Exception {
-	when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.HIGH_PRESSURE_TRESHOLD - 1);
-	alarm.check();
-	when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.HIGH_PRESSURE_TRESHOLD + 1);
-	alarm.check();
-	when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.HIGH_PRESSURE_TRESHOLD);
-	alarm.check();
+    assertTrue(alarm.isAlarmOn());
+  }
 
-	assertTrue(alarm.isAlarmOn());
-    }
+  @Test
+  public void alarmShouldBeOnOnceTriggeredLow() throws Exception {
+    when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.LOW_PRESSURE_TRESHOLD + 1);
+    alarm.check();
+    when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.LOW_PRESSURE_TRESHOLD - 1);
+    alarm.check();
+    when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.LOW_PRESSURE_TRESHOLD);
+    alarm.check();
 
-    @Test
-    public void alarmShouldBeOnOnceTriggeredLow() throws Exception {
-	when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.LOW_PRESSURE_TRESHOLD + 1);
-	alarm.check();
-	when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.LOW_PRESSURE_TRESHOLD - 1);
-	alarm.check();
-	when(sensor.popNextPressurePsiValue()).thenReturn(Alarm.LOW_PRESSURE_TRESHOLD);
-	alarm.check();
-
-	assertTrue(alarm.isAlarmOn());
-    }
+    assertTrue(alarm.isAlarmOn());
+  }
 }
